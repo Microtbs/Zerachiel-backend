c5abdb84a555b9b9f151fe48c36ba28803fcaba1
Mvtt <matt.esposito@proton.me>
2025-06-30 18:21:24 +0200

    chore: add @nestjs/mapped-types dependency

--- backend/src/modules/graves/entities/graves.entity.ts
+++ backend/src/modules/graves/entities/graves.entity.ts
@@ -1,34 +1,40 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
-import { Caretaker } from "src/modules/caretakers/entities/caretakers.entity";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Caretaker } from 'src/modules/caretakers/entities/caretakers.entity';
 
-
-@Entity("graves")
+@Entity('graves')
 export class Grave {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    latitude: number
+  @Column()
+  latitude: number;
 
-    @Column()
-    longitude: number
+  @Column()
+  longitude: number;
 
-    @Column()
-    status: boolean
+  @Column()
+  status: boolean;
 
-    @Column()
-    stonemason: string
+  @Column()
+  stonemason: string;
 
-    @Column()
-    flowers_count: number
+  @Column()
+  flowers_count: number;
 
-    @Column()
-    section: string
+  @Column()
+  section: string;
 
-    @Column()
-    caretakers_id: number
-
-    @ManyToOne(() => Caretaker, (caretakers) => caretakers.graves, { onDelete: 'SET NULL', nullable: true })
-    caretakers: Caretaker;
-
+  @ManyToOne(() => Caretaker, (caretakers) => caretakers.graves, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'caretaker_id' }) // nome della colonna di join nella tabella 'graves'
+  caretakers: Caretaker;
 }
+
