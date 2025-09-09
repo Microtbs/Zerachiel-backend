dc428b7e9034df31953b5ec989b3b4b91855e7c0
microtbs <mirco.98@hotmail.com>
2025-09-09 12:57:38 +0200

    Fix connection

--- src/configs/ormconfig.ts
+++ src/configs/ormconfig.ts
@@ -1,18 +1,19 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
+
 config();
 
 export const ormConfig: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT) || 5432,
-  username: process.env.DATABASE_USERNAME || 'postgres',
-  password: process.env.DATABASE_PASSWORD || '',
-  database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
+  ssl: true,
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
   migrations: [__dirname + '/../migrations/*.{ts,js}'],
   synchronize: false,
   logging: false,
 };
 
-export default new DataSource(ormConfig);
+const dataSource = new DataSource(ormConfig);
+
+export default dataSource;
+
