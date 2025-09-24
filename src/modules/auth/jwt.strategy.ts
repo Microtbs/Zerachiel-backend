1bb41375d5b82e34371abefdd288065c7da23c81
Giovanni Spanò <giovannispano0802@gmail.com>
2025-09-24 12:56:37 +0200

    feat(auth): enhance JWT validation to include additional user information

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { jwtConfig } from '../../configs/jwtconfig';
+import { first } from 'rxjs';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,6 +14,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
+    return {
+      userId: payload.sub,
+      email: payload.email,
+      first_name: payload.first_name,
+      last_name: payload.last_name,
+      tax_code: payload.tax_code,
+      family_member: payload.family_member,
+    };
   }
 }
