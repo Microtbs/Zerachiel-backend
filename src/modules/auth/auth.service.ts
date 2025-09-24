2690043308d0e2e9b3c88b4a8200d64a13f54ebc
Giovanni Spanò <giovannispano0802@gmail.com>
2025-09-24 13:12:07 +0200

    feat(auth): simplify JWT payload in login response and validation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,7 +4,6 @@ import { UsersService } from '../users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
-import { first } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -39,16 +38,15 @@ export class AuthService {
     const isValid = await bcrypt.compare(dto.password, user.password);
     if (!isValid) throw new UnauthorizedException('Credenziali errate');
 
-    const payload = {
-      sub: user.id,
-      email: user.email,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      family_member: user.family_member,
-      tax_code: user.tax_code,
-    };
+    const payload = { sub: user.id, email: user.email };
     return {
       access_token: this.jwtService.sign(payload),
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      tax_code: user.tax_code,
+      family_member: user.family_member,
     };
   }
 }
--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -14,13 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return {
-      userId: payload.sub,
-      email: payload.email,
-      first_name: payload.first_name,
-      last_name: payload.last_name,
-      tax_code: payload.tax_code,
-      family_member: payload.family_member,
-    };
+    return { userId: payload.sub, email: payload.email };
   }
 }
