6ddc064337752987fddf6cc2ac785476d35ea37d
Mvtt <matt.esposito@proton.me>
2025-10-02 11:32:10 +0200

    feat(municipalities): add new column and relationship - OneToOne with municipality_contacts - Add new Column : address & createdAt

--- src/modules/municipalities/dto/create-municipality.dto.ts
+++ src/modules/municipalities/dto/create-municipality.dto.ts
@@ -1,8 +1,13 @@
-import { IsNotEmpty, IsString, Length,} from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateMunicipalityDto {
   @IsString() // verifichiamo che il dato sia una stringa
-  @IsNotEmpty() // verifichiamo che la stringa non sia vuota 
+  @IsNotEmpty() // verifichiamo che la stringa non sia vuota
   @Length(2, 255) // imponiamo una lunghezza minima e massima della stringa
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 255)
+  address: string;
 }
--- src/modules/municipalities/entities/municipality.entity.ts
+++ src/modules/municipalities/entities/municipality.entity.ts
@@ -1,17 +1,30 @@
 import { MunicipalityContact } from '../../municipality_contacts/entities/municipality_contacts.entity';
-import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToOne,
+} from 'typeorm';
 
 @Entity('municipalities')
 export class Municipality {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ type: 'varchar', length: 255 })
   name: string;
 
-  @OneToOne(
-    () => MunicipalityContact,
-    (municipality_contacts) => municipality_contacts.id,
-  )
-  municipality_contacts: MunicipalityContact;
+  @Column({ type: 'varchar', length: 255 })
+  address: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'NOW()',
+  })
+  createdAt: Date;
+
+  @OneToOne(() => MunicipalityContact, (contact) => contact.municipality)
+  contact: MunicipalityContact;
 }
