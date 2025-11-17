6714a0b6f52ee0a27f6fedbe91f5828df1f42af2
Microtbs <210916933+Microtbs@users.noreply.github.com>
2025-11-17 14:56:46 +0100

    feat(mail): refactor mail configuration into a separate MailConfig class

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -2,26 +2,15 @@ import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service';
+import { MailConfig } from '../../configs/mailconfig';
 
 @Module({
   imports: [
     MailerModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        transport: {
-          host: config.get('MAIL_HOST'),
-          port: Number(config.get('MAIL_PORT')),
-          secure: config.get('MAIL_SECURE') === 'true',
-          auth: {
-            user: config.get('MAIL_USER'),
-            pass: config.get('MAIL_PASS'),
-          },
-        },
-        defaults: {
-          from: config.get('MAIL_FROM'),
-        },
-      }),
       inject: [ConfigService],
+      useFactory: (config: ConfigService) =>
+        MailConfig.createMailerOptions(config),
     }),
   ],
   providers: [MailService],
